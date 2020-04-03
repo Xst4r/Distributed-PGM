Added additional parameters for graphtype, tol and gtol, tried doing fisher matrix computation but this did not work as planned - ended up fixing bugs in px instead as well as trying to find out a way to compute the fisher matrix with px.

Code/main.py
@@ -3,6 +3,7 @@ from src.model.model import Susy as SusyModel
 from src.preprocessing.sampling import Random
 from src.conf.settings import CONFIG
 from src.data.dataset import CoverType, Susy, Dota2
+from src.data.metrics import fisher_information
 from time import time
 import os
 import argparse
@@ -142,7 +143,7 @@ class Coordinator(object):
         methods = ['mean', 'radon', 'wa', 'kl', 'var']
         aggregates = {k: [] for k in methods}
         sample_size = np.ceil(
-            (distributed_models.sample_func(distributed_models.train_counter) * distributed_models.data_delta) / 2)
+            (distributed_models.sample_func(distributed_models.epoch) * distributed_models.data_delta) / 2)
         if weights is not None:
             aggr = [Mean(weights),
                     RadonMachine(weights, self.r, self.h),
@@ -202,28 +203,14 @@ class Coordinator(object):
                                                    h=1,
                                                    d=data.train.shape[0])
                     self.r = r
-
+                    fisher_information(trained_model.px_model[0])
                     # theta_samples, theta_old = self.sample_parameters(trained_model)
                     # theta_arr = np.concatenate(theta_samples, axis=1)
-                    """
-                    # This only works when variance stays the same.
-                    else:
-                        if theta_old[0].shape[0] == trained_model.px_model[0].weights.shape[0]:
-                            for j in range(len(theta_samples)):
-                                theta_samples[j] = (theta_samples[j] - np.outer(theta_old[j], np.ones(shape=theta_samples[j].shape[1]))) + \
-                                                   np.outer(trained_model.px_model[j].weights[:,None],  np.ones(shape=theta_samples[j].shape[1]))
-                                theta_old[j] = trained_model.px_model[j].weights[:,None]
-                                theta_arr = np.concatenate(theta_samples, axis=1)
-                        else:
-                            theta_samples, theta_old = self.sample_parameters(trained_model)
-                            theta_arr = np.concatenate(theta_samples, axis=1)
-                    """
 
-                    # TODO: Generate new Model Parameters here
                     # Aggregation
                     logger.info("Aggregating Model No. " + str(i))
                     kl_samples = [np.ascontiguousarray(
-                        data.train.iloc[idx][:trained_model.data_delta * trained_model.train_counter].values,
+                        data.train.iloc[idx][:trained_model.data_delta * trained_model.epoch].values,
                         dtype=np.uint16) for idx in sampler.split_idx]
                     local_predictions = None
                     local_predictions = trained_model.predict(n_test=self.n_test)
@@ -243,6 +230,8 @@ class Coordinator(object):
                                                  None, kl_samples)
                     self.record_progress(aggregation, model.n_local_data, i, self.experiment_path, local_acc)
                     aggregates[model.n_local_data] = aggregation
+                    if model.n_local_data > sampler.split_idx[0].shape[0]:
+                        break
                 self.write_progress(os.path.join(self.experiment_path, str(i)), "accuracy.csv")
                 models.append(trained_model)
                 model.write_progress_hook(path=os.path.join(self.experiment_path, str(i)),
@@ -465,6 +454,20 @@ def parse_args():
                         help="Hyperparameter for the Hoefding Bound. Probability of suff. stats. having at most"
                              "distance of HoefdingDistance",
                         default=0.5)
+    parser.add_argument('--gtol',
+                        type=float,
+                        help="Stopping criterion for the prox. gradient descent based on the gradient norm.",
+                        default=1e-7)
+    parser.add_argument('--tol',
+                        type=float,
+                        help="Stopping criterion for the prox. gradient descent based on objective rate of change.",
+                        default=1e-5)
+
+    parser.add_argument('--graphtype',
+                        type=str,
+                        help="GraphType for Probabilistic Graphical Models",
+                        choices=["chain", "tree", "full"],
+                        default="tree")
     args = parser.parse_args()
     return args
 

Code/src/data/metrics.py
@@ -52,22 +52,31 @@ def sk_confusion_matrix(y_true, y_pred):
 
 def fisher_information(model):
     mu, A = model.infer()
+    mu = mu[:model.weights.shape[0]]
     states = np.copy(model.states)
     edgelist = np.copy(model.graph.edgelist)
 
     x = np.ascontiguousarray(np.zeros(edgelist.shape[0] + 1 , dtype=np.uint16) - 1)
+    test_x = np.ascontiguousarray(np.zeros(edgelist.shape[0] + 1 , dtype=np.uint16))
     fisher_matrix = None
-    offset = 0
-    for u, v in edgelist:
+    offset = np.insert(np.cumsum([states[u] * states[v] for u,v in edgelist]), 0, 0)
+    node_offset = np.insert(np.cumsum(states), 0, 0)
+    for idx, (u, v) in enumerate(edgelist):
         for i in range(states[u]):
             for j in range(states[v]):
                 x[u] = i
                 x[v] = j
-                mu_i, _ = model.infer(observed=x)
-                phi_x = model.phi(x)
-                assert phi_x[offset + i*states[u] + j] == 1
-                fisher_matrix = mu_i * mu[offset + i*states[u] + j] if fisher_matrix is None else np.hstack((fisher_matrix, mu_i * mu[offset + i*j]))
-        offset += states[u] * states[v]
+                mu_, _ = model.infer(observed=x)
+                mu_ij = mu_[:model.weights.shape[0]]
+                prob_uv = model.prob(u, i, v, j)
+                try:
+                    if not (prob_uv == model.prob(u, i, v, j)):
+                        print("Error: " + str(prob_uv) +  str(model.prob(u, i, v, j)))
+                    model.prob(-1, i, 100, j)
+                    model.prob(-1, 500, 100, 300)
+                except (IndexError, ValueError):
+                    print("Error: " + "e= (" + str(u) + ',' + str(v) + ")" + " state: " + str(i) + " " + str(j))
+                fisher_matrix = mu_ij * prob_uv if fisher_matrix is None else np.vstack((fisher_matrix, mu_ij * prob_uv))
         x[u] = -1
         x[v] = -1
     return fisher_matrix

Code/src/data/synthetic.py
@@ -1,5 +1,6 @@
 import numpy as np
-from src.conf.settings import ROOT_DIR
+from src.conf.settings import CONFIG
+from src.data.metrics import fisher_information
 import pandas as pd
 import os
 import pxpy as px
@@ -24,11 +25,11 @@ class DataGenerator(object):
 
 
 def main():
-    n_dim = 20
-    n_classes = 10
+    n_dim = 5
+    n_classes = 2
     mean = [np.random.rand(n_dim) * np.random.randint(1, 6) for i in range(n_classes)]
     cov =  [np.random.rand(n_dim, n_dim) * np.random.randint(1, 4) for i in range(n_classes)]
-    gen =  DataGenerator(100000, 10, np.random.multivariate_normal, mean, cov)
+    gen =  DataGenerator(5000, 2, np.random.multivariate_normal, mean, cov)
     return gen.sample()
 
 
@@ -41,7 +42,18 @@ if __name__ == '__main__':
 
     res = np.ascontiguousarray(res, dtype=np.float64)
     disc, M = px.discretize(res, 10)
-    new_data = os.path.join(ROOT_DIR, "data")
+    model = px.train(disc, graph=px.GraphType.auto_tree)
+    fi = fisher_information(model)
+    phis = []
+    for d in disc:
+        phis.append(model.phi(d))
+    mu, a = model.infer()
+    mu = mu[:vars]
+    vars = model.weights.shape[0]
+    cov_XY = np.cov(np.array(phis).T)
+    EX_EY = np.outer(mu, mu)
+    E_XY = cov_XY + EX_EY
+    new_data = os.path.join(CONFIG.ROOT_DIR, "data")
     os.chdir(new_data)
     os.mkdir("SYNTH")
     df = pd.DataFrame(disc)

Code/src/model/model.py
@@ -236,9 +236,13 @@ class Model:
                              mode=CONFIG.MODELTYPE,
                              initial_stepsize=1e-2,
                              opt_regularization_hook=CONFIG.REGULARIZATION,
-                             inference=px.InferenceType.junction_tree,
                              k=4)
             self.reset_train()
+
+            phis = []
+            for d in data:
+                phis.append(model.phi(d).astype(np.uint8))
+
             if len(split) > 1:
                 self.px_batch_local[self.epoch][i] = model
                 model = self.merge_weights(model)
@@ -359,11 +363,11 @@ class Model:
     def opt_proximal_hook(self, state_p):
         return
 
-    def check_convergence(self, curr_obj, grad, tol=1e-5, gtol=1e-7):
+    def check_convergence(self, curr_obj, grad):
         if CONFIG.MODELTYPE == px.ModelType.integer:
-            return self.prev_obj - curr_obj < tol
+            return self.prev_obj - curr_obj < CONFIG.TOL
         else:
-            return self.prev_obj - curr_obj < tol and np.linalg.norm(grad, np.infty) < gtol
+            return self.prev_obj - curr_obj < CONFIG.TOL and np.linalg.norm(grad, np.infty) < CONFIG.GTOL
 
     def parallel_train(self, split=None):
         # This is slow and bad, maybe distribute proc   esses among devices.
@@ -415,7 +419,7 @@ class Model:
             when creating this object.
         """
         holdout = np.ascontiguousarray(self.data_set.holdout.to_numpy().astype(np.uint16))
-        self.edgelist = px.train(data=holdout, graph=px.GraphType.auto_tree, iters=1, mode=CONFIG.MODELTYPE,
+        self.edgelist = px.train(data=holdout, graph=CONFIG.GRAPHTYPE, iters=1, mode=CONFIG.MODELTYPE,
                                  opt_regularization_hook=CONFIG.REGULARIZATION).graph.edgelist
         self.graph = self._px_create_graph()
         self.weights = self.init_weights()