fixed a bug in covariance matrix generation when a small numerical error would prevent a randm cov to be created.

Code/main.py
@@ -338,9 +338,13 @@ class Coordinator(object):
         return np.diag(np.ones(n_dim)) * eps
 
     def gen_random_cov(self, n_dim):
-        eigs = self.random_state.rand(n_dim)
-        eigs = eigs / np.sum(eigs) * eigs.shape[0]
-        return random_correlation.rvs(eigs, random_state=self.random_state)
+        try:
+            eigs = self.random_state.rand(n_dim)
+            eigs = eigs / np.sum(eigs) * eigs.shape[0]
+            return random_correlation.rvs(eigs, random_state=self.random_state)
+        except Exception as e:
+            cov = self.random_state.randn(n_dim, n_dim)
+            return np.dot(cov, cov.T) / n_dim
 
     def gen_fisher_cov(self, phi, mu):
         return np.outer(mu - phi, (mu - phi).T)
@@ -470,7 +474,7 @@ if __name__ == '__main__':
 
     keywords = ['--data', '--covtype', '--reg', '--graphtype']
     datasets = ['covertype', 'dota2', 'susy']
-    sample_parameters = ['none', 'unif', 'random', 'fish']
+    sample_parameters = ['random']
     reg = ['None', 'l2']
     configurations = [element for element in itertools.product(*[datasets, sample_parameters, reg])]
     func = lambda x: zip(keywords, x)
@@ -507,6 +511,7 @@ if __name__ == '__main__':
             result, agg = coordinator.prepare_and_run()
         except Exception as e:
             with open("exceptions.txt", "a+") as file:
+                logger.error("Experiment Failed in " + str(cmd_args.data)  + " "+ str(cmd_args.reg) + " " + str(cmd_args.covtype) + "\n")
                 file.write("Experiment Failed in " + str(cmd_args.data)  + " "+ str(cmd_args.reg) + " " + str(cmd_args.covtype) + "\n")
                 file.write(str(e) + "\n")
 

Code/src/data/metrics.py
@@ -42,7 +42,7 @@ def sk_accuracy(y_true, y_pred):
 
 def sk_f1_score(y_true, y_pred):
     if sklearn_available:
-        if np.unique(y_true).shape[0] > 2:
+        if np.unique(y_true).shape[0] > 2 or np.unique(y_pred).shape[0] > 2:
             return f1_score(y_true, y_pred, average='macro')
         else:
             return f1_score(y_true, y_pred)