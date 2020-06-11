7cba9765a039381b46b4d2e60faa07e98c1f155c
Danny Heinrich <danny.heinrich@udo.edu>
2020-06-11 10:09:23 +0200

    plots and experiment loading

--- Code/src/io/load_results.py
+++ Code/src/io/load_results.py
@@ -62,29 +62,69 @@ def plot_obj(obj):
 def load_results(experiment):
     baseline_stats = {}
     agg_stats = []
+    test_ll = []
     objs = None
+    test_objs = None
     for cv_split in os.listdir(experiment):
+
+        # -------------------------------
+        # Load Baseline Results
+        # -------------------------------
         if cv_split == 'baseline':
             b_path = os.path.join(experiment, cv_split)
             baseline_stats['baseline_metrics'] =  pd.read_csv(os.path.join(b_path, "baseline_metrics.csv"))
+
+        # -------------------------------
+        # Load CV Results
+        # -------------------------------
         elif os.path.isdir(os.path.join(experiment, cv_split)):
             curr_results = {}
             a_path = os.path.join(experiment, cv_split)
+
+            # Load Accuracy
             curr_results['acc'] = pd.read_csv(os.path.join(a_path, "accuracy.csv"))
+
+            # Load F1 Score
             curr_results['f1'] = pd.read_csv(os.path.join(a_path, "f1.csv"), header=None, names=curr_results['acc'].columns)
+
+            # -------------------------------
+            # Likelihood evaluated on Baseline
+            #
+            # File is obj.csv
+            # -------------------------------
             if cv_split == '0':
                 curr_results['obj'] = pd.read_csv(os.path.join(a_path, "obj.csv"))
                 objs = curr_results['obj'][0:10]
                 objs.columns = objs.columns.str.strip()
-                curr_results['obj'] = curr_results['obj'][10:]
             else:
-                curr_results['obj'] = pd.read_csv(os.path.join(a_path, "obj.csv"), header= None)
+                curr_results['obj'] = pd.read_csv(os.path.join(a_path, "obj.csv"))
                 curr_results['obj'].columns = agg_stats[0]['obj'].columns
+
             curr_results['obj'].columns = curr_results['obj'].columns.str.strip()
+            curr_results['obj']['obj'] = curr_results['obj']['obj'].apply(float)
+            curr_results['obj'] = curr_results['obj'][10:]
+
+            # -------------------------------
+            # Likelihood evaluated on Test Split
+            #
+            # File is test_likelihood.csv
+            # -------------------------------
+            if cv_split == '0':
+                curr_results['test_ll'] = pd.read_csv(os.path.join(a_path, "test_likelihood.csv"))
+                test_ll = curr_results['test_ll'][0:10]
+                test_ll.columns = test_ll.columns.str.strip()
+            else:
+                curr_results['test_ll'] = pd.read_csv(os.path.join(a_path, "test_likelihood.csv"))
+                curr_results['test_ll'].columns = agg_stats[0]['test_ll'].columns
+
+            curr_results['test_ll'].columns = curr_results['test_ll'].columns.str.strip()
+            curr_results['test_ll']['test_ll'] = curr_results['test_ll']['test_ll'].apply(float)
+            curr_results['test_ll'] = curr_results['test_ll'][10:]
+
             agg_stats.append(curr_results)
         else:
             pass
-    return baseline_stats, agg_stats, objs
+    return baseline_stats, agg_stats, objs, test_ll
 
 
 def main(path):
@@ -94,15 +134,16 @@ def main(path):
     eps = [1e-1, 5e-2]
     configurations = [element for element in itertools.product(*[reg, eps])]
     for experiment in os.listdir(path):
-        stats = pd.read_csv(os.path.join(path, experiment, "readme.md"), header=None, sep=":")
-        stats.index = stats[0].str.strip()
-        results.append((stats, load_results(os.path.join(path, experiment))))
+        if experiment!= "old":
+            stats = pd.read_csv(os.path.join(path, experiment, "readme.md"), header=None, sep=":")
+            stats.index = stats[0].str.strip()
+            results.append((stats, load_results(os.path.join(path, experiment))))
     m_accs = []
     m_f1 = []
     m_objs = []
     exp_obj = []
     for stats, result in results:
-        baseline, local, objs = result
+        baseline, local, objs, test_ll = result
         accs = []
         f1 = []
         norm_objs = []
@@ -119,7 +160,10 @@ def main(path):
         for loc_obj in norm_objs:
             avg_obj = loc_obj['obj'].to_numpy() if avg_obj is None else avg_obj + loc_obj['obj'].to_numpy()
             avg_norm_obj = loc_obj['normalized_obj'].to_numpy() if avg_norm_obj is None else avg_norm_obj + loc_obj['normalized_obj'].to_numpy()
-        tmp = pd.concat([loc_obj['n_data'], loc_obj['name'], pd.Series(avg_obj/len(norm_objs)), pd.Series(avg_norm_obj/len(norm_objs))], axis=1, keys=[n for n in loc_obj.columns])
+        tmp = pd.concat([loc_obj['n_data'].reset_index(drop=True), loc_obj['name'].reset_index(drop=True),
+                         pd.Series(avg_obj/len(norm_objs)),
+                         pd.Series(avg_norm_obj/len(norm_objs))],
+                        axis=1, keys=[n for n in loc_obj.columns])
         tmp.columns = loc_obj.columns
         m_objs.append(tmp)
         exp_obj.append(norm_objs)
@@ -137,4 +181,5 @@ def main(path):
 
 if __name__ == '__main__':
     for folder in os.listdir(EXPERIMENT_DIR):
-        main(os.path.join(EXPERIMENT_DIR, folder))
+        if folder=='covertype':
+            main(os.path.join(EXPERIMENT_DIR, folder))
